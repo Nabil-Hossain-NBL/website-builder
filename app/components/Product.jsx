5b1da53f84fa156fa6c4b500c4f4d627535de0af
Nabil-Hossain-NBL <nabilhossainprf@gmail.com>
2024-02-26 22:23:18 +0600

    dynamic data setup

--- app/components/Product.jsx
+++ app/components/Product.jsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
 import icon5 from "@/public/icon5.svg";
+import FiveStar from "@/public/FiveStar.png";
+import FourStar from "@/public/FourStar.png";
+import FourHalfStar from "@/public/FourHalfStar.png";
 
 async function Product({ product }) {
     console.log(product, 'hi from product');
@@ -9,17 +12,18 @@ async function Product({ product }) {
             <div className="grid grid-cols-4 gap-4 bg-white rounded-xl">
                 <div className="w-5/6 mx-auto">
                     <Image src={product.img} alt="" width={141} height={103} layout="responsive" />
+                    <p className="text-center">{product.name}</p>
                 </div>
 
-                <div className="col-span-2">
-                    <h1>WixPro 72-Inch Responsive Website Builder- omprehensive Digital Platform Creation Tool, Streamlined Design Interface for Professional Websites and Online Stores</h1>
+                <div className="col-span-2 my-2">
+                    <h1> <span className="font-bold">{product.title}</span> {product.description}</h1>
 
-                    <h1>Main highlights</h1>
-                    <p>
-                        [What You Get]: Receive the WixPro website builder suite, access to premium design templates, an extensive library of widgets and apps, and detailed step-by-step guides.
+                    <h1 className="font-bold my-2">Main highlights</h1>
+                    <p className="ml-4">
+                        [What You Get]: {product.highlights}
                     </p>
 
-                    <p className="flex">
+                    <p className="flex mt-3 text-[#1B88F4]" >
                         Show more <Image src={icon5} alt="" />
                     </p>
 
@@ -29,22 +33,26 @@ async function Product({ product }) {
                 <div className="bg-white">
                     <div className="mx-auto text-center text h-full grid grid-rows-2 ">
                         <div className="bg-[#F3F9FF] w-2/4 mx-auto rounded-b-xl pt-2">
-                            <div>
-                                9.5
+                            <div className="text-[2em]">
+                                {product.rating}
                             </div>
                             <div>
                                 Exceptional
                             </div>
-                            <div></div>
+                            <div className="flex justify-center">
+                                {
+                                    product.star === 5 && <Image src={FiveStar} alt="" /> || product.star === 4 && <Image src={FourStar} alt="" />
+                                }
+                            </div>
                         </div>
                         <div className="flex justify-center items-end">
-                            <button className="bg-[#1B88F4] text-white w-4/6 py-2 rounded-xl">View</button>
+                            <button className="bg-[#1B88F4] text-white w-4/6 py-2 rounded-xl mb-2">View</button>
                         </div>
                     </div>
 
                 </div>
                 <div className="w-10 rounded-full bg-white text-center absolute -ml-5 mt-4 p-2  border">
-                    1
+                    {product.id}
                 </div>
             </div>
 
